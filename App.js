458c3e39b0fe79a5211c3f7f0247f7ab0afa155d
Maiav27 <44206589+Maiav27@users.noreply.github.com>
2022-03-14 08:19:42 -0300

    slider

--- App.js
+++ App.js
@@ -1,13 +1,69 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import AppIntroSlider from 'react-native-app-intro-slider';
 
 export default function App() {
+  const [showHome, setShowHome] =useState(false)
+
+  const slides = [
+    {
+      key : 1 ,
+      title : 'Naruto',
+      text : 'Jovem orfão, que no futuro se tornara o 7 hokage',
+      image : require('./src/assets/Naruto.png')
+    },
+    {
+      key : 2,
+      title : 'Luffy',
+      text : 'Pirata que estica, quer se torna o rei dos piratas',
+      image : require('./src/assets/Luffy.jpeg')
+    },
+    {
+      key : 3,
+      title : 'Hinata Shoyo',
+      text : 'menino pequeno que pula alto, quer se torna o pequeno gigante no vôlei',
+      image : require('./src/assets/Hinata2.png')
+    }
+  ]
+
+  const renderSlides = ({item}) =>{
+    return(
+      <View >
+        <Image source={item.image}  style={{resizeMode : 'cover', 
+          height : '400px',
+          width : '100%' ,}}/>
+        <Text> 
+          {item.title}
+        </Text>
+        <Text>
+          {item.text}
+        </Text>
+
+          
+      </View>
+    )
+
+  }
+
+  if(showHome) {
+    return(
+     <Text>
+        Entrou na Home
+     </Text>
+    )
+  } else{
+
+  
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <AppIntroSlider renderItem={renderSlides}
+    data={slides}
+    activeDotStyle={{
+      backgroundColor : '#009CFF', 
+      width : 30 }}/>
+    
   );
+  }
 }
 
 const styles = StyleSheet.create({
